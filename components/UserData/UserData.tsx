2db3df3866389c6f26d656d05637761a335ab380
dlwhd990 <dlwhd990@gmail.com>
2023-01-14 01:10:32 +0900

    userData 버튼들 다시 복구 (모바일에서 해당 버튼들 사용 불가능해짐)

--- components/UserData/UserData.tsx
+++ components/UserData/UserData.tsx
@@ -1,8 +1,25 @@
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCloudMoon, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import profileImage from "../../public/images/d.gif";
+import { useAppDispatch } from "../../store/hooks";
+import { changeDarkModeState, closeSidebar } from "../../store/toggle";
 import styles from "./UserData.module.css";
 
 const UserData = () => {
+  const dispatch = useAppDispatch();
+
+  const dispatchCloseSidebar = () => {
+    dispatch(closeSidebar());
+  };
+
+  const darkModeHandler = () => {
+    dispatch(changeDarkModeState());
+  };
+
   return (
     <div className={styles.user}>
       <div className={styles.data_container}>
@@ -10,7 +27,7 @@ const UserData = () => {
         <p className={styles.name}>이종혁</p>
         <p className={styles.desc}>DEV 🐻</p>
       </div>
-      {/* <div className={styles.button_container}>
+      <div className={styles.button_container}>
         <Link href="/">
           <button onClick={dispatchCloseSidebar}>
             <FontAwesomeIcon icon={faHouse} className={styles.icon_home} />
@@ -24,7 +41,7 @@ const UserData = () => {
         <button onClick={darkModeHandler}>
           <FontAwesomeIcon icon={faCloudMoon} className={styles.icon_dark} />
         </button>
-      </div> */}
+      </div>
     </div>
   );
 };
