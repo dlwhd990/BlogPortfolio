32d7c36de94279e2f1c48c111309250eacc79804
dlwhd990 <dlwhd990@gmail.com>
2023-01-13 18:33:22 +0900

    article 페이지 SSG 적용

--- pages/article/[articleId].tsx
+++ pages/article/[articleId].tsx
@@ -1,56 +1,58 @@
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { ObjectId } from "mongodb";
+import { GetStaticPropsContext } from "next";
 import ArticleBanner from "../../components/ArticleBanner/ArticleBanner";
-import Loader from "../../components/Loader/Loader";
+import Article from "../../model/article";
 import styles from "../../styles/articlePage.module.css";
+import { connectToDatabase } from "../../util/mongodb";
 
-const ArticlePage = () => {
-  const router = useRouter();
-  const [pending, setPending] = useState(true);
-  const [article, setArticle] = useState({
-    _id: new Object(),
-    title: "",
-    content: "",
-    date: 0,
-    category: "",
-    menu: "",
-    commentCount: 0,
-    likeCount: 0,
-    coverImage: "",
-    previewContent: "",
-  });
-
-  useEffect(() => {
-    if (!router.isReady) return;
-    setPending(true);
-    const getArticle = async () => {
-      const response = await axios.get(
-        `/api/article/${router.query.articleId}`
-      );
-      if (response.data.success) {
-        setArticle(response.data.result);
-        setPending(false);
-      }
-    };
-    getArticle();
-  }, [router.isReady, router.query.articleId]);
-
+const ArticlePage: React.FC<{ article: Article }> = ({ article }) => {
   return (
     <main className={styles.main}>
-      {pending && <Loader />}
-      {!pending && (
-        <>
-          <ArticleBanner article={article} />
-          <section className={styles.article_section}>
-            <article
-              dangerouslySetInnerHTML={{ __html: article.content }}
-            ></article>
-          </section>
-        </>
-      )}
+      <ArticleBanner article={article} />
+      <section className={styles.article_section}>
+        <article
+          dangerouslySetInnerHTML={{ __html: article.content }}
+        ></article>
+      </section>
     </main>
   );
 };
+
+export async function getStaticPaths() {
+  const db = await connectToDatabase();
+  const collection = db.collection("article");
+  const result = await collection.distinct("_id", {});
+
+  const paths = result.map((item) => {
+    return {
+      params: {
+        articleId: item.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const db = await connectToDatabase();
+  const collection = db.collection("article");
+  const article = await collection.findOne({
+    _id: new ObjectId(
+      typeof context?.params?.articleId === "string"
+        ? context?.params?.articleId
+        : ""
+    ),
+  });
+
+  return {
+    props: {
+      article: JSON.parse(JSON.stringify(article)),
+    },
+  };
+}
 
 export default ArticlePage;
