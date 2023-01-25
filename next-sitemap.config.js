f0f41a52c547b09ec2b4310d531139af2e296efa
dlwhd990 <dlwhd990@gmail.com>
2023-01-25 16:21:16 +0900

    next-sitemap config 추가가

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const config = {
+  siteUrl: "https://blog.dlwhd990.vercel.app",
+  generateRobotsTxt: true,
+  exclude: ["/sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://blog.dlwhd990.vercel.app/sitemap.xml"],
+  },
+};
